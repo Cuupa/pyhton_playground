e51032caada9a1ef77ae64e65a2dfc196ae78ba9
Cuupa <simon.thiel2+github@gmail.com>
2019-11-09 15:09:47 +0100

    added rudimentairy notification

--- de/cuupa/pricescraper.py
+++ de/cuupa/pricescraper.py
@@ -1,6 +1,5 @@
-import os
-
 import csv
+import os
 import sys
 
 import requests
@@ -18,13 +17,44 @@ alias = {"B075X181C5": "Seagate Ironwolf 12 TB",
 row_names = ["Name", "Price"]
 
 
+def is_lowes_price(url):
+    path, name = os.path.split(url)
+    filename = alias.get(name) + '.csv'
+    with open(filename, 'r', newline='\n') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        lowest_price = float('inf')
+        list_prices = list()
+        length = 0
+        for row in reader:
+            if row[1] != 'Price':
+                length += 1
+                price = float(row[1].split()[0].replace(',', '.'))
+                list_prices.append(price)
+                if price < lowest_price:
+                    lowest_price = price
+
+        if list_prices[length - 1] == lowest_price:
+            return True
+    return False
+
+
+def get_message_text(url):
+    path, name = os.path.split(url)
+    return alias.get(name)
+
 def main():
     for url in urls:
-        request = requests.get(url, stream=False, headers={'User-agent': 'Mozilla/5.0'})
-        if request.status_code != 200:
-            print("Error getting result: HTTP/" + str(request.status_code))
-        else:
-            process_article(request, url)
+        # get_prices_write_to_csv(url)
+        if is_lowes_price(url):
+            print("Price dropped for " + get_message_text(url))
+
+
+def get_prices_write_to_csv(url):
+    request = requests.get(url, stream=False, headers={'User-agent': 'Mozilla/5.0'})
+    if request.status_code != 200:
+        print("Error getting result: HTTP/" + str(request.status_code))
+    else:
+        process_article(request, url)
 
 
 def process_article(request, url):
@@ -33,10 +63,10 @@ def process_article(request, url):
         path, name = os.path.split(url)
         filename = alias.get(name) + '.csv'
         if not os.path.exists(filename):
-            csv.writer(open(filename, 'a', newline=''), quoting=csv.QUOTE_NONE, delimiter=';', quotechar='',
+            csv.writer(open(filename, 'a', newline='\n'), quoting=csv.QUOTE_NONE, delimiter=';', quotechar='',
                        escapechar='\\').writerow(row_names)
 
-        csv.writer(open(filename, 'a', newline=''), quoting=csv.QUOTE_NONE, delimiter=';', quotechar='',
+        csv.writer(open(filename, 'a', newline='\n'), quoting=csv.QUOTE_NONE, delimiter=';', quotechar='',
                    escapechar='\\').writerow([name, final_price])
 
     except:
