b7bcc76515b54bd15b969b3ea9324053162c3518
Cuupa <simon.thiel2+github@gmail.com>
2019-11-12 14:52:24 +0100

    added the .chm files....

--- src/scraper/MSDNMagazineScraper.py
+++ src/scraper/MSDNMagazineScraper.py
@@ -4,12 +4,14 @@ from util.Download import Download
 
 url: str = "https://docs.microsoft.com/en-us/archive/msdn-magazine/msdn-magazine-issues"
 download_url: str = "http://download.microsoft.com/download/"
-url_pattern: str = "^(http:\\/\\/download.microsoft.com\\/download[\\/0-9a-f\\-]*[pdf]{1})"
+url_pattern_PDFs: str = "^(http:\\/\\/download.microsoft.com\\/download[\\/0-9a-f\\-]*[pdf]{1})"
+url_pattern_CHMs: str = "^(http:\\/\\/download.microsoft.com\\/download[\\/0-9a-f\\-]*[chm]{1})"
 path_to_save: str = os.path.curdir + "download" + os.path.sep
 
 
 def main():
-    Download(url, download_url, path_to_save, url_pattern).download()
+    Download(url, download_url, path_to_save, url_pattern_PDFs).download()
+    Download(url, download_url, path_to_save, url_pattern_CHMs).download()
 
 
 main()
